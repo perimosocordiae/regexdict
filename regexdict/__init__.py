26a5c7c6759ab90688aa7c44d52828e8af8672da
CJ Carey <perimosocordiae@gmail.com>
2015-08-31 15:08:10 -0500

    refactor: avoid duplication with py3k version

--- regexdict/__init__.py
+++ regexdict/__init__.py
@@ -1,15 +1,3 @@
-import sys
-from collections import namedtuple
-
-if sys.version_info[0] == 2:
-  from regexdict import regexdict, keys, values, iterkeys, itervalues
-else:
-  from .regexdict_py3k import regexdict, keys, values
-  iterkeys, itervalues = keys, values
+from regexdict import regexdict, return_types
 
 __all__ = ['regexdict', 'return_types']
-
-_RT = namedtuple('ReturnType', ('list', 'dict', 'keys', 'values',
-                                'iterkeys', 'itervalues'))
-
-return_types = _RT(list, dict, keys, values, iterkeys, itervalues)
--- regexdict/regexdict.py
+++ regexdict/regexdict.py
@@ -3,65 +3,59 @@
 CJ Carey - perimosocordiae@github
 Daryl Koopersmith - koop@github, darylkoop.com
 """
-
 import re
+import sys
+from collections import namedtuple
 
+regex_type = type(re.compile(''))
 iterkeys = lambda seq: (k for k, v in seq)
 itervalues = lambda seq: (v for k, v in seq)
-keys = lambda seq: list(iterkeys(seq))
-values = lambda seq: list(itervalues(seq))
-rtype = type(re.compile(''))
+if sys.version_info[0] == 2:
+    keys = lambda seq: list(iterkeys(seq))
+    values = lambda seq: list(itervalues(seq))
+    str_type = basestring
+else:
+    keys, values = iterkeys, itervalues
+    str_type = str
+
+_RT = namedtuple('ReturnType', ('list', 'dict', 'keys', 'values',
+                                'iterkeys', 'itervalues'))
+return_types = _RT(list, dict, keys, values, iterkeys, itervalues)
 
 
 class regexdict(dict):
 
-    @staticmethod
-    def _unslice(key):
-        if type(key) is slice:
-            # re.compile won't re-compile a regex :-)
-            return key.start, re.compile(key.stop)  # omg hax
-        return None, None
-
-    @staticmethod
-    def _search(regex, s):
-        try:
-            return regex.search(s)
-        except TypeError:
-            return None
+    def __filter_matches(self, regex):
+        return (k for k in self if _is_match(regex, k))
 
     def __contains__(self, key):
-        if type(key) == rtype:
-            return any(True for k in self.iterkeys()
-                       if regexdict._search(key, k))
-        else:
+        if not isinstance(key, regex_type):
             return dict.__contains__(self, key)
+        return any(True for _ in self.__filter_matches(key))
 
     def __getitem__(self, key):
-        rettype, regex = regexdict._unslice(key)
-        if regex:
-            kv_iter = ((k, v) for k, v in self.iteritems()
-                       if regexdict._search(regex, k))
-            return rettype(kv_iter) if rettype else kv_iter
-        else:
+        rettype, regex = _unslice(key)
+        if regex is None:
             return dict.__getitem__(self, key)
-
-    def __apply_to_matches(self, func, *args):
-        _, regex = regexdict._unslice(args[0])
-        if regex:
-            for k in self.iterkeys():
-                if regexdict._search(regex, k):
-                    func(self, *args)
-        else:
-            if type(args[0]) is str:
-                func(self, *args)
+        kv_iter = ((k, dict.__getitem__(self, k))
+                   for k in self.__filter_matches(regex))
+        return rettype(kv_iter) if rettype else kv_iter
 
     def __setitem__(self, key, value):
-        self.__apply_to_matches(dict.__setitem__, key, value)
+        _, regex = _unslice(key)
+        if regex is None:
+            return dict.__setitem__(self, key, value)
+        for k in self.__filter_matches(regex):
+            dict.__setitem__(self, k, value)
 
     def __delitem__(self, key):
-        self.__apply_to_matches(dict.__delitem__, key)
+        _, regex = _unslice(key)
+        if regex is None:
+            return dict.__delitem__(self, key)
+        for k in self.__filter_matches(regex):
+            dict.__delitem__(self, k)
 
-    # Python2 compat:
+    # Python2 compatibility functions
     def __getslice__(self, start, stop):
         return self.__getitem__(slice(start, stop))
 
@@ -70,3 +64,17 @@ class regexdict(dict):
 
     def __delslice__(self, start, stop):
         return self.__delitem__(slice(start, stop))
+
+
+def _unslice(key):
+    if isinstance(key, slice):
+        # slices have the form ret_type:regex
+        return key.start, re.compile(key.stop)
+    return None, None
+
+
+def _is_match(regex, s):
+    try:
+        return regex.search(s) is not None
+    except TypeError:
+        return False
